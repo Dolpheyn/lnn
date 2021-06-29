662239749102eeec83d20e039c1fd631ac39245f
Dolpheyn <farissufyan99@gmail.com>
2021-06-29 12:40:22 +0800

    Routes: Add /register, /login, /customers

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CustomerController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::resource('customers', CustomerController::class);
+
+/**
+  * For routes that require authentication
+Route::middleware('auth:api')->group( function () {
+
 });
+**/
