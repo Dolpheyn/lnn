bd5265eb330edfc9e4a3b4e191b4210832f1a954
Dolpheyn <farissufyan99@gmail.com>
2021-07-08 15:27:33 +0800

    Factories: Add DelivererFactory

---
+++ database/factories/DelivererFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Deliverer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DelivererFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Deliverer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+      return [
+        'name' => $this->faker->name(),
+        'email' => $this->faker->unique()->safeEmail(),
+        'phoneNumber' => $this->faker->unique()->phoneNumber()
+      ];
+    }
+}
