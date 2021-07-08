8cea35d69b83d229103e67f3d7342b1411fc4b65
Dolpheyn <farissufyan99@gmail.com>
2021-07-08 17:25:13 +0800

    seeders: Add DelivererSeeder

---
+++ database/seeders/DelivererSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Deliverer;
+use Illuminate\Database\Seeder;
+
+class DelivererSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      Deliverer::factory()
+        ->count(5)
+        ->create();
+    }
+}
