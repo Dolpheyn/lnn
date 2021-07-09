571b0976e11b5fe53d1289f1aba5ee92e8c425fd
Dolpheyn <farissufyan99@gmail.com>
2021-07-09 16:22:27 +0800

    CustomerSeeder: Set 1 random address for each user to be active

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -16,9 +16,16 @@ class CustomerSeeder extends Seeder
     public function run()
     {
       // Generate 5 customers with 2 addresses each.
-      Customer::factory()
+      $customers = Customer::factory()
         ->count(5)
         ->hasAddresses(2)
         ->create();
+
+      // Set 1 random address to be the active address.
+      foreach($customers as $customer) {
+        $randomAddress = $customer->addresses->random();
+        $randomAddress->isActive = 1;
+        $randomAddress->save();
+      }
     }
 }
